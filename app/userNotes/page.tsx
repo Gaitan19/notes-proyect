e58d8efbc1d4e1f51ea9fdea59204ba44207425a
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2024-03-25 12:10:14 -0600

    added validations

--- app/userNotes/page.tsx
+++ app/userNotes/page.tsx
@@ -1,14 +1,22 @@
 import HeaderNotes from '@/components/HeaderNotes';
 import NotesList from '@/components/NotesList';
 import { cookies } from 'next/headers';
+import { redirect } from "next/navigation";
+
 
 export default function UserNotesPage() {
   const cookieStore = cookies();
 
   const data = cookieStore.get('sb-yavhtdnzcvaymaykpmju-auth-token');
 
+  if (!data) {
+    return redirect("/login");
+  }
+
   const userString = data?.value;
   const { user } = userString ? JSON.parse(userString) : null;
+
+  
 
   return (
     <>
